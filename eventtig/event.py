cd4b98a388cd2a2b61ee016df3430ca822af0442
James <james.baster@opendataservices.coop>
2021-03-20 13:36:01 +0000

    Parse and store start and end

--- eventtig/event.py
+++ eventtig/event.py
@@ -1,3 +1,6 @@
+import re
+import datetime
+import pytz
 
 
 class Event:
@@ -10,3 +13,36 @@ class Event:
         self.description = data.get('description')
         self.id = id
         self.tag_ids = data.get('tags')
+        self.start_year, self.start_month, self.start_day, self.start_hour, self.start_minute = self._parse_string_to_datetime(data.get('start'))
+        self.end_year, self.end_month, self.end_day, self.end_hour, self.end_minute = self._parse_string_to_datetime(data.get('end'))
+
+    def _parse_string_to_datetime(self, value):
+        m = re.search('([0-9][0-9][0-9][0-9])-([0-9][0-9])-([0-9][0-9]) ([0-9][0-9]):([0-9][0-9])', value)
+        year = int(m.group(1))
+        month = int(m.group(2))
+        day = int(m.group(3))
+        hour = int(m.group(4))
+        minute = int(m.group(5))
+        return [year, month, day, hour, minute]
+
+    def get_start_epoch(self):
+        start = datetime.datetime(
+            self.start_year,
+            self.start_month,
+            self.start_day,
+            self.start_hour,
+            self.start_minute,
+            tzinfo=pytz.timezone('Europe/London')
+        )
+        return start.timestamp()
+
+    def get_end_epoch(self):
+        end = datetime.datetime(
+            self.end_year,
+            self.end_month,
+            self.end_day,
+            self.end_hour,
+            self.end_minute,
+            tzinfo=pytz.timezone('Europe/London')
+        )
+        return end.timestamp()
--- eventtig/sqlite.py
+++ eventtig/sqlite.py
@@ -12,7 +12,19 @@ class DataStoreSQLite:
         cur.execute('''CREATE TABLE event (
             id TEXT PRIMARY KEY, 
             title text, 
-            description text
+            description text,
+            start_year integer,
+            start_month integer,
+            start_day integer,
+            start_hour integer,
+            start_minute integer,
+            start_epoch integer,
+            end_year integer,
+            end_month integer,
+            end_day integer,
+            end_hour integer,
+            end_minute integer,
+            end_epoch integer
             )'''
         )
         cur.execute('''CREATE TABLE tag (
@@ -36,8 +48,27 @@ class DataStoreSQLite:
             event.id,
             event.title,
             event.description,
+            event.start_year,
+            event.start_month,
+            event.start_day,
+            event.start_hour,
+            event.start_minute,
+            event.get_start_epoch(),
+            event.end_year,
+            event.end_month,
+            event.end_day,
+            event.end_hour,
+            event.end_minute,
+            event.get_end_epoch()
         ]
-        cur.execute("INSERT INTO event (id, title, description) VALUES (?, ?, ?)", insert_data)
+        cur.execute(
+            """INSERT INTO event (
+            id, title, description,
+            start_year, start_month,start_day,start_hour,start_minute,start_epoch,
+            end_year,end_month,end_day,end_hour,end_minute,end_epoch
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+            insert_data
+        )
         for tag_id in event.tag_ids:
             cur.execute("INSERT INTO event_has_tag (event_id, tag_id) VALUES (?, ?)", [ event.id, tag_id ])
         self.connection.commit()
